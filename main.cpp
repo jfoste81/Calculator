879a673d71b1a7609bdee02e42295e4a3547ee95
Joshua Foster <jfoste81@uncc.edu>
2023-03-23 10:46:27 -0400

    combinational math logic is wrong

--- main.cpp
+++ main.cpp
@@ -10,47 +10,57 @@ int main(){
     vector<double> nums;
     vector<char> operators;
     double result;
+    string willBeANumber = "";
 
     cout << "Enter an equation: ";
     getline(cin, equation);
 
-    for(int i = 0; i < equation.size(); i++){
-        //make the character add to a string until it reaches a space -> then convert that string to an int and add it to the nums vector
-        //then add the operator to the operators vector
-        char c = equation[i];
-        if(equation[i] == '+' || equation[i] == '-' || equation[i] == '*' || equation[i] == '/'){
+    for(int i = 0; i < equation.length(); i++){
+        if(isdigit(equation[i])){
+            willBeANumber += equation[i];
+        } else if(equation[i] == '+' || equation[i] == '-' || equation[i] == '*' || equation[i] == '/'){
             operators.push_back(equation[i]);
-        }
-        else if(isdigit(equation[i])){
-            nums.push_back(atoi(&equation[i]));
+        } 
+        if((!isdigit(equation[i]) && willBeANumber.size() > 0) || i == equation.length() - 1){
+            nums.push_back(stod(willBeANumber));
+            willBeANumber = "";
         }
     }
 
-    for(int i : nums){
+    for(int i : nums){ // REMOVE THIS SECTION LATER
+        cout << i << endl;
+    }
+    for(char i : operators){ // REMOVE THIS SECTION LATER
         cout << i << endl;
     }
 
-    for(int i = 0; i < nums.size(); i++){
-        double temp = 0;
+    int size = nums.size();
+    for(int i = 0; i <= size; i++){
+        // double temp = 0;
         if(operators[i] == '+'){
-            cout << "Adding " << nums[i] << " and " << nums[i+1] << endl;
-            temp = nums[i] + nums[i+1];
+            i++;
+            cout << "Adding " << nums[0] << " and " << nums[i] << endl;
+            nums[0] += nums[i];
         }
         else if(operators[i] == '-'){
-            cout << "Subtracting " << nums[i] << " and " << nums[i+1] << endl;
-            temp = nums[i] - nums[i+1];
+            i++;
+            cout << "Subtracting " << nums[0] << " and " << nums[i] << endl;
+            nums[0] -= nums[i];
         }
         else if(operators[i] == '*'){
-            cout << "Multiplying " << nums[i] << " and " << nums[i+1] << endl;
-            temp = nums[i] * nums[i+1];
+            i++;
+            cout << "Multiplying " << nums[0] << " and " << nums[i] << endl;
+            nums[0] *= nums[i];
         }
         else if(operators[i] == '/'){
-            cout << "Dividing " << nums[i] << " and " << nums[i+1] << endl;
-            temp = nums[i] / nums[i+1];
-        } 
-        result += temp;
+            i++;
+            cout << "Dividing " << nums[0] << " and " << nums[i] << endl;
+            nums[0] /= nums[i];
+        } else {
+            cout << "No operator found." << endl;
+        }    
     }
-
+    result += nums[0];
     cout << "Answer: " << result << endl;
     return 0;
 }
